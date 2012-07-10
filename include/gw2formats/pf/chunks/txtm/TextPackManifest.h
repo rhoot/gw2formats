5686cb600ce56499df13de420c40b662b99e37b3
rhoot <johan@skold.cc>
2012-07-10 14:57:32 +0200

    Copy+pasta.

--- include/gw2formats/pf/chunks/txtm/TextPackManifest.h
+++ include/gw2formats/pf/chunks/txtm/TextPackManifest.h
@@ -4,7 +4,7 @@
 
     This file is part of gw2formats.
 
-    Gw2Browser is free software: you can redistribute it and/or modify
+    gw2formats is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
